6143b36e249b699c28f626e59f5092867067630e
Celine-Nova <cocotte243@gmail.com>
2019-01-21 16:31:19 +0100

    copie du fichier de cour pour le challenge

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,28 +15,13 @@ class MainController extends Controller
     {
         //
     }
-    public function index() {
-        return 'Hello world';
-    }
-     // ancienne preconisation (dépréciee) : rajouter le suffix action dans le nom de la methode ex totoAction()...
-     //le type de la variable $request ne sera ni un string, ni un tableau mais une request
-    public function toto(Request $request) {
-        //dd = dump die
-        //dd($request);
-        /*
-         grace à l'objet request directement instancié par Lumen
-         je peux récupérer directement et sans distinction ce qui est passé en POST ou en GET => on oublie le $_GET / $_POST ;)
-        */
-        $chiffre = $request->input('chiffre', 'valeur_par_defaut_du_parametre');
-        dump($chiffre);
-        // rappel : ma clef chiffre va automatiquement est convertie en variable $chiffre arrivé dans ma vue
+    public function home() {
         return view(
-                'mavuetoto',
-                [
-                    'super_name_nova' => 'Toto le grand',
-                    'test'=> 'youpi une deuxieme variable',
-                    'chiffre'=> $chiffre
-                ]
+            'home',
+            [
+                'test' => 'coucou',
+            ]
         );
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -27,38 +27,6 @@
 //     return 'Hello world';
 //  });
  $router->get('/', [
-    'uses' => 'MainController@index',
-    'as' => 'nom_de_route_index'
-]);
-
-
-//je peux aussi genérer du code HTML directement en retour du routing
-// $router->get('/toto-route', function () use ($router) {
-
-//     return '<h1> Hello Toto </h1>';
-
-// });
-
-// $router->get('/toto-route', function () use ($router) {
-// /*
-//      Dans lumen, la fonction "view()" va automatiquement chercher les vue dans le dossier reservé a cet effet qui est "resources/views"
-//
-//      De ce fait et dans mon cas, le framework ira chercher aumtomatiquement les bonnes vue au bon endroit
-//
-//      NOTE : l'appel au vue s'effectue SANS l'extension php ou autre ( pas de mavuetoto.php)
-//     */
-//     return view(
-//         'mavuetoto',
-//         [
-//             'super_name_nova' => 'Toto le grand',
-//             'test'=> 'youpi une deuxieme variable'
-//         ]
-//     );
-// });
-
-// $router->get('/toto', 'MainController@toto');
-
-$router->get('/toto', [
-    'uses' => 'MainController@toto', // la clef associative "uses" permet de definir le controller + methode a associer à la route "/toto"
-    'as' => 'nom_de_route_toto' // la clef associative "as" permet de definir un nom de route notamment pour la génération des liens dans une page <3
+    'uses' => 'MainController@home',
+    'as' => 'nom_de_route_home'
 ]);
